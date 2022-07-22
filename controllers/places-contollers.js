68c76b18383bd5682683f69960897fb31a6d1386
Jose-Antonio Rubio <jadrubio@gmail.com>
2022-07-22 11:10:13 -0400

    hiding of api keys

--- controllers/places-contollers.js
+++ controllers/places-contollers.js
@@ -58,7 +58,7 @@ const createPlace = async (req, res, next) => {
     next(new HttpError(422, 'Invalid Inputs Passed.'));
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
   let coordinates;
 
   try {
@@ -73,12 +73,12 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator,
+    creator: req.userData.userId,
   });
 
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     const error = new HttpError(500, 'Creating place failed. Please try again');
     return next(error);
